eb7824c0534be879aca49b8b41c21c825fc8b589
AlcheJ <alchej@naver.com>
2026-03-09 14:30:20 +0900

    맵 변화에 따른 BGM 변경
    F1, F2 : 맵 변경(BGM)
    Alpha1 : 스킬효과음 재생

---
+++ Assets/Game/Personal/JongJun/SkillSoundTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SkillSoundTest : MonoBehaviour
+{
+    private AudioSource skillSource;
+
+    private void Awake()
+    {
+        skillSource = gameObject.GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlaySkillSound();
+        }
+    }
+
+    void PlaySkillSound()
+    {
+        if (skillSource.clip != null) skillSource.PlayOneShot(skillSource.clip);
+        else Debug.LogWarning("SkillSound의 AudioSource에 오디오 클립이 할당되지 않았습니다!");
+    }
+}
