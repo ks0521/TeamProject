88506237691c29a10a605f7401fd85b301246264
GAWN GYU <rhksrb0823@gmail.com>
2026-03-10 13:24:06 +0900

    수정중

---
+++ Assets/Game/UI/UI/Scripts/AbilityPop.cs
@@ -0,0 +1,20 @@
+using Game_UI_Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityPop : BasePop
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Game/UI/UI/Scripts/SkillPop.cs
+++ Assets/Game/UI/UI/Scripts/SkillPop.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-public class GoldTest : MonoBehaviour
+public class SkillPop : BasePop
 {
-    [Header("°ñµå")]
-    [Range(0, 10000)]
-    public int testgold;
-
-    public TextMeshProUGUI GoldText;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        GoldText.text = testgold.ToString();
+        
     }
 }
