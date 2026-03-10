253f137c9bb567d9a1bfeaaa9c8f14c5d880cf46
GyuSeong <74663336+ks0521@users.noreply.github.com>
2026-03-10 15:13:36 +0900

    refactor : 데이터 저장 방식 변경
    기존 구조체 -> 클래스 방식(참조타입)으로 변경

--- Assets/Game/Base/Data/PlayerStatCalculator.cs
+++ Assets/Game/Base/Data/PlayerStatCalculator.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Base.Managers;
 using UnityEngine;
 
 namespace Base.Data
 {
+    /// <summary> </summary>
     public class PlayerStatCalculator : MonoBehaviour
     {
-        
+        public void CalcState()
+        {
+            
+        }
     }
 }
