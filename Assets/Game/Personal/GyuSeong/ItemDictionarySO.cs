8d51b1a3d96110e09e494a3af0849cb97e9b8347
GyuSeong <74663336+ks0521@users.noreply.github.com>
2026-03-10 10:08:16 +0900

    기본 데이터셋 생성

---
+++ Assets/Game/Personal/GyuSeong/ItemDictionarySO.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Personal.GyuSeong
+{
+    [CreateAssetMenu(menuName = ("Game/Growth/ItemDictionary"))]
+    public class ItemDictionarySO : ScriptableObject
+    {
+        [Header("아이템 전체")] public List<ItemSO> allItems = new();
+        Dictionary<int, ItemSO> itemDic;
+
+        void MakeDictionary()
+        {
+            itemDic = new Dictionary<int, ItemSO>();
+            foreach (var item in allItems)
+            {
+                itemDic.Add(item.key, item);
+            }
+        }
+
+        public ItemSO GetSO(int key)
+        {
+            if (itemDic == null)
+            {
+                MakeDictionary();
+                Debug.Log("딕셔너리를 생성했습니다. ");
+            }
+
+            if (!itemDic.TryGetValue(key, out var item))
+            {
+                Debug.LogWarning("키에 해당하는 아이템이 없습니다. ");
+                return null;
+            }
+
+            Debug.Log($"{item.itemName}");
+            return item;
+        }
+    }
+}
