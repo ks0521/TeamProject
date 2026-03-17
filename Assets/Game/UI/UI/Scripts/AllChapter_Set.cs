0bafa788d5a23161b57196cb705fda9abc8ff32a
GAWN GYU <rhksrb0823@gmail.com>
2026-03-17 18:42:12 +0900

    3월 17일

---
+++ Assets/Game/UI/UI/Scripts/AllChapter_Set.cs
@@ -0,0 +1,36 @@
+﻿using Base.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AllChapter_Set : MonoBehaviour
+{
+    [Header("챕터")]
+    [SerializeField] Chapter_1_Set chapter1;
+    [SerializeField] Chapter_2_Set chapter2;
+    [SerializeField] Chapter_3_Set chapter3;
+    [SerializeField] Chapter_4_Set chapter4;
+
+    [Header("스테이지 매니저")]
+    [SerializeField] StageManager stageManager;
+
+    private void Awake()
+    {
+        if (stageManager == null)
+        {
+            stageManager = FindFirstObjectByType<StageManager>();
+        }
+    }
+    private void OnEnable()
+    {
+        AllChapter();
+    }
+    void AllChapter()
+    {
+        chapter1.SetChapter1(stageManager,1);
+        
+
+    }
+
+}
---
+++ Assets/Game/UI/UI/Scripts/UI_Set/ChapterStage_Set/Chapter_1_Set.cs
@@ -0,0 +1,27 @@
+﻿using Base.Data;
+using Base.Managers;
+using Base.Save;
+
+using Personal_Jongjun;
+using System.Collections;
+using System.Collections.Generic;
+using UI.Scripts.Stage;
+using UnityEngine;
+
+
+public class Chapter_1_Set : MonoBehaviour
+{
+    [SerializeField] Stage_Set[] stages;
+    public void SetChapter1(StageManager stageManager , int chapterNum)
+    {
+        for (int i = 0; i < stages.Length; i++)
+        {
+            int stageNum = i + 1;
+
+            Base.Managers.StageEntry entry = stageManager.GetStageEntry(chapterNum, stageNum);
+            stages[i].SetStage(entry);
+        }
+    }
+}
+
+
