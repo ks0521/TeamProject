cfe7e1f95f60f5089b4c85a3f58ffcb6ba57b525
GAWN GYU <rhksrb0823@gmail.com>
2026-03-18 18:40:43 +0900

    3월 18일 작업
    구조개선중(스크립트가 너무 쓸데없이 많은 거 같아서 한개의 스크립트로 stat 타입을 다르게 설정하는 방향으로)
    
    StatEntry 쪽에서 데이터 받아와서 UI 쪽으로 연결중
    
    스테이지 상태에 따라 버튼 상태 변경(이미지로 표시)

--- Assets/Game/UI/UI/Scripts/Ability.cs
+++ Assets/Game/UI/UI/Scripts/Ability.cs
@@ -8,154 +8,106 @@ using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System;
 
 namespace UI.Scripts.Ability
 {
     public class Ability : MonoBehaviour
     {
         [Header("매니저")]
-        [SerializeField] GameManager gameManager;
-        public enum StatusType
-        {
-            Atk, MaxHp, Def, AtkSpeed, CritChance, CritDmg, MoveSpeed, GoldRate, ExpRate, ItemDropRate
-        }//나중에 참고
+        [SerializeField] private GameDataManager gameManager;
+
+        [Header("스텟 UI 목록")]
+        [SerializeField] StatItemView[] statItemViews;
+        
         [Header("능력치 구매 버튼")]
-        [SerializeField] Button[] Upbtn;
+        [SerializeField] private Button[] Upbtn;
 
         [Header("곱하기 버튼")]
-        [SerializeField] Button_Set btnX;
+        [SerializeField] private Button_Set btnX;
         private enum XState
         {
-            X1, X10, X100
+            X1 , X10 , X100
         }
         private XState X_state;
         private float multiValue;
 
-        [Header("UI 참조")]
-        [SerializeField] Atk_Set atk;
-        [SerializeField] MaxHp_Set maxHp;
-        [SerializeField] Def_Set def;
-        [SerializeField] AtkSpeed_Set atkspeed;
-        [SerializeField] CritChance_Set critChance;
-        [SerializeField] CritDmg_Set critDmg;
-        [SerializeField] MoveSpeed_Set moveSpeed;
-        [SerializeField] GoldRate_Set goldRate;
-        [SerializeField] ExpRate_Set expRate;
-        [SerializeField] ItemDropRate_Set itemDropRate;
 
-      
         // Start is called before the first frame update
         public void OnEnable()
         {
-            ReFreshUI();
+            ReFreshAllUI();
         }
         private void Start()
         {
-            atk.BindLevelUp(OnClickAtkLevelUp);
-            maxHp.BindLevelUp(OnClickMaxHPLevelUp);
-            def.BindLevelUp(OnClickDefLevelUp);
-            atkspeed.BindLevelUp(OnClickAtkSpeedLevelUp);
-            critChance.BindLevelUp(OnClickCritChanceLevelUp);
-            critDmg.BindLevelUp(OnClickCritDmgLevelUp);
-            moveSpeed.BindLevelUp(OnClickMoveSpeedLevelUp);
-            goldRate.BindLevelUp(OnClickGoldRateLevelUp);
-            expRate.BindLevelUp(OnClickExpRateLevelUp);
-            itemDropRate.BindLevelUp(OnClickItmeDropRateLevelUp);
-
-            ReFreshUI();
+            BindAllButtons();
+            ReFreshAllUI();
             ChangeState(XState.X1);
         }
-        public void ReFreshUI()
+        private StatItemView GetType(StatusType type)
         {
-            
-        }//능력치팝업창 UI 갱신용 함수(능력치 팝업창 안에있는 UI 갱신용 함수 추가 예정)
+            foreach (var view in statItemViews)
+            {
+                if (view.statusType == type)
+                    return view;
+            }
 
-        private void OnClickAtkLevelUp()
+            return null;
+        }//타입에 해당하는 StatItemView 찾아주는 함수
+        private void BindAllButtons()
         {
-        }//테스트용(나중에 수정할 예정)
-        private void OnClickMaxHPLevelUp()
-        {
-
+            foreach (var stat in statItemViews)
+            {
+                StatusType type = stat.statusType;
+                stat.BindLevelUp(() => OnClickLevelUp(type));
+            }
         }
-        private void OnClickDefLevelUp()
+        public void ReFreshAllUI()
         {
-
+            foreach (var stat in statItemViews)
+            {
+                ReFreshStatUI(stat.statusType);
+            }
         }
-        private void OnClickAtkSpeedLevelUp()
+        public void ReFreshStatUI(StatusType type)
+        {
+            if (!gameManager.statusConfig.TryGetStatEntry(type, out var statEntry))
+            {
+                Debug.Log($"{type} : statEntry를 찾지 못함");
+                return;
+            }//null 방지
+
+            int playerLevel = gameManager.runtimeData.currencyData.level;
+            int playerGold = gameManager.runtimeData.currencyData.gold;
+            int currentLevle = gameManager.runtimeData.stat.upgrade[type];
+
+            float currentValue = currentLevle * statEntry.increasePerEnhance; //현재 스텟 수치
+            float nextValue = (currentLevle + 1) * statEntry.increasePerEnhance; //증가 후 스텟 수치
+            int cost = currentLevle * statEntry.enhanceCost; //스텟 가격 부분
+
+            bool isUnLock = playerLevel <= statEntry.unlockLevel;
+            bool canLevelUp = isUnLock && playerGold >= cost && currentLevle < statEntry.maxLevel;
+
+            StatItemView itemView = GetType(type);
+
+            if (itemView == null)
+            {
+                Debug.Log($"{type}에 해당되는 스텟 UI가 없음");
+            }
+
+            itemView.RefreshUI(statEntry , currentLevle , currentValue , nextValue ,cost , canLevelUp , isUnLock);
+                
+
+        }//능력치팝업창 UI 갱신용 함수
+
+        private void OnClickLevelUp(StatusType type)
         {
 
-        }
-        private void OnClickCritChanceLevelUp()
-        {
-
-        }
-        private void OnClickCritDmgLevelUp()
-        {
-
-        }
-        private void OnClickMoveSpeedLevelUp()
-        {
-
-        }
-        private void OnClickGoldRateLevelUp()
-        {
-
-        }
-        private void OnClickExpRateLevelUp()
-        {
-
-        }
-        private void OnClickItmeDropRateLevelUp()
-        {
-
-        }
+        } // 미구현
 
 
-        private void RefreshAtkUI(StatusType type)
-        {
-            
-           
 
-            
-                        
-            
-        }//테스트용(나중에 수정할 예정)
-        private void RefreshMaxHPUI()
-        {
 
-        }
-        private void RefreshDef()
-        {
-
-        }
-        private void RefreshAtkSpeedUI()
-        {
-
-        }
-        private void RefreshCritChanceUI()
-        {
-
-        }
-        private void RefreshCritDmgUI()
-        {
-
-        }
-        private void RefreshMoveSpeed()
-        {
-
-        }
-        private void RefreshGoldRate()
-        {
-
-        }
-        private void RefreshExpRate()
-        {
-
-        }
-        private void RefreshItemDropRate()
-        {
-
-        }
 
         void ChangeState(XState newState)
         {
@@ -178,7 +130,7 @@ namespace UI.Scripts.Ability
                     btnX.SelectButton(2);
                     break;
             }
-            ReFreshUI();
+            ReFreshAllUI();
         }//상태 전환 함수
         public void OnClickX1()
         {
@@ -196,7 +148,7 @@ namespace UI.Scripts.Ability
         // Update is called once per frame
         private void Update()
         {
-            
+
         }
     }
 
---
+++ Assets/Game/UI/UI/Scripts/UI_Set/AbilityUI_Set/StatItemView.cs
@@ -0,0 +1,81 @@
+﻿using Base.Save;
+using Battle;
+using Growth.StatUpgrade;
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+using UnityEngine.UI;
+public class StatItemView : MonoBehaviour
+{
+    [Header("담당 타입")]
+    [SerializeField] public StatusType statusType;
+
+    [Header("UI 참조")]
+    [SerializeField] private TextMeshProUGUI statsNameText;
+    [SerializeField] private TextMeshProUGUI statsLevelText;
+    [SerializeField] private TextMeshProUGUI currentStats;
+    [SerializeField] private TextMeshProUGUI nextStats;
+    [SerializeField] private TextMeshProUGUI levelupcost;
+    [SerializeField] private Image costImage; 
+    [SerializeField] private Button levelUpButton;
+
+    [Header("잠금 UI")]
+    [SerializeField] private GameObject lockPanel;
+    [SerializeField] private TextMeshProUGUI unlockLevelText;
+
+
+    public void RefreshUI(StatEntry statEntry , int currentLevel, float currentValue ,float nextValue , int cost , bool canLevelUp , bool isUnlock)
+    {
+        levelUpButton.interactable = canLevelUp;
+        if (canLevelUp)
+        {
+            levelUpButton.image.color = Color.yellow;
+        }
+        else
+        {
+            levelUpButton.image.color = Color.gray;
+        }
+        lockPanel.SetActive(!isUnlock);
+        unlockLevelText.text = $"Lv : {statEntry.unlockLevel}";
+
+        statsNameText.text = statEntry.type.ToString();
+
+        if (currentLevel < statEntry.maxLevel)
+        {
+            statsLevelText.color = Color.white;
+            currentStats.color = Color.white;
+            nextStats.enabled = true;
+            levelupcost.enabled = true;
+            costImage.enabled = true;
+            levelUpButton.gameObject.SetActive(true);
+
+            statsLevelText.text = currentLevel.ToString();
+            currentStats.text = currentValue.ToString();
+            nextStats.text = nextValue.ToString();
+            levelupcost.text = cost.ToString();
+        }
+        else
+        {
+            statsLevelText.color = Color.yellow;
+            currentStats.color = Color.yellow;
+            nextStats.enabled = false;
+            levelupcost.enabled = false;
+            costImage.enabled = false;
+            levelUpButton.gameObject.SetActive(false);
+
+            statsLevelText.text = "Lv : MAX";
+            currentStats.text = currentValue.ToString();
+        }
+    }//스텟 UI 표시
+    public void BindLevelUp(Action action)
+    {
+        levelUpButton.onClick.RemoveAllListeners();
+        levelUpButton.onClick.AddListener(() => action?.Invoke());
+    }//버튼 OnClick 에 함수 넣어주는 함수
+
+}
