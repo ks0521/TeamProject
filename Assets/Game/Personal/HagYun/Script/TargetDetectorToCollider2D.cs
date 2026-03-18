454bfb0df51347f2e58f39638ab6dbbe9fbc512c
khy13 <kihy0103@gmail.com>
2026-03-18 13:24:06 +0900

    temp - EquipSkillController 분리용 push
    - EquipSkillController
      - PlayerEquipSkillController 스크립트로 player용 auto skill control 기능 분리
    - 기타
      - Hagyun Scene에서 사용 가능하도록 조정
      - TargetDetector 변경중

--- Assets/Game/Personal/HagYun/Script/TargetDetectorToCollider2D.cs
+++ Assets/Game/Personal/HagYun/Script/TargetDetectorToCollider2D.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetDetectorUsingCircleCollider2D : MonoBehaviour
+namespace Personal.HagYun
 {
-    HashSet<Collider2D> detectedTarget = new HashSet<Collider2D>();
-    public bool IsDetectedTarget => detectedTarget.Count > 0;
-    public int DetectedTargetCnt => detectedTarget.Count;
-    [SerializeField] LayerMask lm;
-    [SerializeField] CircleCollider2D col;
-    private void OnTriggerEnter2D(Collider2D collision)
+    public class TargetDetectorUsingCircleCollider2D : MonoBehaviour
     {
-        if ((lm.value & (1 << collision.gameObject.layer)) != 0)
+        HashSet<Collider2D> detectedTarget = new HashSet<Collider2D>();
+        public bool IsDetectedTarget => detectedTarget.Count > 0;
+        public int DetectedTargetCnt => detectedTarget.Count;
+        [SerializeField] LayerMask lm;
+        [SerializeField] CircleCollider2D col;
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            detectedTarget.Add(collision);
+            if ((lm.value & (1 << collision.gameObject.layer)) != 0)
+            {
+                detectedTarget.Add(collision);
+            }
         }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        detectedTarget.Remove(collision);
-    }
-    public void SetCollider(CircleCollider2D col) => this.col = col;
-    public void TargetCheckToUpdate(bool isAtking = false)
-    {
-        if (IsDetectedTarget)
+        private void OnTriggerExit2D(Collider2D collision)
         {
-            //if (Time.frameCount % 10 != 0) return; // 10프레임 일 때만 통과
-            //if (isAtking) return; // isAtking이 false일 때만 통과
-            detectedTarget.RemoveWhere(e => e == null || !e.gameObject.activeInHierarchy);
+            detectedTarget.Remove(collision);
         }
-    }
-    void ColliderRadiusChange(float radius)
-    {
-        col.radius = radius;
+        public void SetCollider(CircleCollider2D col) => this.col = col;
+        public void TargetCheckToUpdate(bool isAtking = false)
+        {
+            if (IsDetectedTarget)
+            {
+                //if (Time.frameCount % 10 != 0) return; // 10프레임 일 때만 통과
+                //if (isAtking) return; // isAtking이 false일 때만 통과
+                detectedTarget.RemoveWhere(e => e == null || !e.gameObject.activeInHierarchy);
+            }
+        }
+        public void ColliderRadiusChange(float radius)
+        {
+            col.radius = radius;
+        }
     }
 }
