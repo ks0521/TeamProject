85bcea58cdfc1af4e0b45e93c25ac01bb94f1011
GyuSeong <74663336+ks0521@users.noreply.github.com>
2026-03-18 20:31:11 +0900

    style : 코드 가독성 향상
    기존 데이터 관리부분 네이밍이 이해하기 어렵게 되어있어 명칭 변경

--- Assets/Game/Base/Save/GameSaveData.cs
+++ Assets/Game/Base/Save/GameSaveData.cs
@@ -8,79 +8,79 @@ namespace Base.Save
     [Serializable]
     public class GameSaveData
     {
-        public StageProgressData stageProgress = new();
-        public PlayerCurrencyData currencyData = new();
-        public PlayerItemInventoryData itemInventory = new();
+        public StageProgressState stage = new();
+        public PlayerCurrencyState currency = new();
+        public PlayerItemInventoryState itemInventory = new();
         //public PlayerEquipmentInventoryData equipmentInventory = new(); mvp에선 미구현
         //public PlayerEquipmentData equipment = new(); mvp에선 미구현
-        public PlayerStatUpgradeData stat = new();
-        public PlayerSkillData skill = new();
-        public PlayerAccessTimeData lastAccess = new();
+        public PlayerStatUpgradeState statUpgrades = new();
+        public PlayerSkillState skillProgress = new();
+        public LastSessionTime lastAccess = new();
     }
-    [Serializable]public class StageProgressData
+    [Serializable]public class StageProgressState
     {
         public int selectedNormalStage; //직전 일반스테이지
         public int selectedNormalChapter; //현재 일반 스테이지
         public int nextChallangeStage; //도전 가능한 스테이지
         public int nextChallangeChapter; //도전 가능한 챕터
     }
-    [Serializable]public class PlayerCurrencyData
+    [Serializable]public class PlayerCurrencyState
     {
         public int level;
         public float exp;
         public int gold;
         public int statStone;
     }
-    [Serializable]public class PlayerItemInventoryData
+    [Serializable]public class PlayerItemInventoryState
     {
-        public List<ItemEntry> items;
+        public List<ItemEntry> owneditemCounts;
     }
-    [Serializable]public class PlayerEquipmentInventoryData
+    [Serializable]public class PlayerEquipmentInventoryState
     {
-        public List<EquipmentEntry> equipments;
+        public List<EquipmentEntry> equipmentEntries;
     }
-    [Serializable]public class PlayerEquipmentData
+    [Serializable]public class PlayerEquipmentState
     {
-        public int equipWepon; //장비의 키를 저장
-        public int equipArmor;
-        public int equipAccessory;
+        public int equippedWeponKey; //장비의 키를 저장
+        public int equippedArmorKey;
+        public int equippedAccessoryKey;
     }
-    [Serializable]public class PlayerStatUpgradeData
+    [Serializable]public class PlayerStatUpgradeState
     {
-        public List<StatusEntry> upgrade; //statusType은 StatusSo.cs에 존재
+        public List<StatusEntry> upgradeLevelsByType; //statusType은 StatusSo.cs에 존재
     }
-    [Serializable]public class PlayerSkillData
+    [Serializable]public class PlayerSkillState
     {
         public int skillSlots; //나중에 스킬슬롯 객체 만들면 수정
-        public List<SkillEntry> skills; //key : 스킬 키, value : 해당 스킬 레벨
+        public List<SkillEntry> skillProgressState; //key : 스킬 키, value : 해당 스킬 레벨
     }
     [Serializable]public class EquipmentEntry
     {
         public int key; //아이템 키
-        public int enforce; //강화 수치
-        public int count; //개수
+        public int enhancementLevel; //강화 수치
+        public int ownedCount; //개수
         public bool isDiscovered; //해금 여부
     }
     [Serializable]
     public struct ItemEntry
     {
         public int key; //아이템 키
-        public int count; //아이템 개수
+        public int ownedCount; //아이템 개수
     }
     [Serializable]
     public struct StatusEntry
     {
-        public StatusType type; //스탯 종류
-        public int count; //강화 수치
+        public StatusType statType; //스탯 종류
+        public int enhancementLevel; //강화 수치
     }
     [Serializable]
     public struct SkillEntry
     {
         public int key; //스킬 키
-        public int count; //강화 수치
+        public int enhancementCount; //강화 수치
     }
     [Serializable]
-    public class PlayerAccessTimeData
+    public class LastSessionTime
     {
         public long lastConnectTime;
     }
--- Assets/Game/Growth/StatUpgrade/Scripts/StatusCalculator.cs
+++ Assets/Game/Growth/StatUpgrade/Scripts/StatusCalculator.cs
@@ -17,23 +17,22 @@ public class StatusCalculator : MonoBehaviour
     }
 
     /// <summary> 런타임 데이터를 반영해 플레이어 최종스펙 수정</summary>
-    /// <param name="runData"></param>
-    public void Calculate(RuntimeData runData)
+    /// <param name="runProgressState"></param>
+    public void Calculate(RuntimeProgressState runProgressState)
     {
         Debug.Log("계산 시작");
-        StatEntry stat;
-        if (statusConfig.TryGetStatEntry(StatusType.Atk, out stat))
+        if (statusConfig.TryGetStatEntry(StatusType.Atk, out StatEntry stat))
         {   //최종 공격력 = 기본 공격력 + 공격력 스탯 업그레이드 수 * 업그레이드 당 증가량
             runtimeStatus.finalBattleStatus.atk =
                 runtimeStatus.baseStat.baseBattle.atk +
-                runData.stat.upgrade[StatusType.Atk] * stat.increasePerEnhance;
+                runProgressState.statUpgrades.upgradeLevelsByType[StatusType.Atk] * stat.increasePerEnhance;
         }
         else{ Debug.LogWarning($"공격력 SO 찾지 못함");}
         if (statusConfig.TryGetStatEntry(StatusType.MaxHp, out stat))
         {   //최종 최대체력 = 기본 최대체력 + 최대체력 스탯 업그레이드 수 * 업그레이드 당 증가량
             runtimeStatus.finalBattleStatus.maxHp =
                 runtimeStatus.baseStat.baseBattle.maxHp +
-                runData.stat.upgrade[StatusType.MaxHp] * stat.increasePerEnhance;
+                runProgressState.statUpgrades.upgradeLevelsByType[StatusType.MaxHp] * stat.increasePerEnhance;
             //플레이어 hp도 동일한 양 증가시켜주기
         }
         else{ Debug.LogWarning($"최대체력 SO 찾지 못함");}
@@ -42,7 +41,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 방어력 = 기본 방어력 + 방어력 스탯 업그레이드 수 * 업그레이드 당 증가량
             runtimeStatus.finalBattleStatus.def =
                 runtimeStatus.baseStat.baseBattle.def +
-                runData.stat.upgrade[StatusType.Def] * stat.increasePerEnhance;
+                runProgressState.statUpgrades.upgradeLevelsByType[StatusType.Def] * stat.increasePerEnhance;
         }
         else{ Debug.LogWarning($"방어력 SO 찾지 못함");}
 
@@ -50,7 +49,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 공격속도 = 기본 공격속도 - (공격스택 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalBattleStatus.atkSpeed =
                 runtimeStatus.baseStat.baseBattle.atkSpeed -
-                (runData.stat.upgrade[StatusType.AtkSpeed] * stat.increasePerEnhance);
+                (runProgressState.statUpgrades.upgradeLevelsByType[StatusType.AtkSpeed] * stat.increasePerEnhance);
             runtimeStatus.finalBattleStatus.atkSpeed = Mathf.Clamp(runtimeStatus.finalBattleStatus.atkSpeed, 0.1f, 3f); //공격속도 증가 디버프 있을수도 있어서
         }
         else{ Debug.LogWarning($"공격속도 SO 찾지 못함");}
@@ -59,7 +58,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 크리티컬 확률 = 기본 크리티컬 확률 + (크리티컬 확률 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalBattleStatus.critChance =
                 runtimeStatus.baseStat.baseBattle.critChance +
-                runData.stat.upgrade[StatusType.CritChance] * stat.increasePerEnhance;
+                runProgressState.statUpgrades.upgradeLevelsByType[StatusType.CritChance] * stat.increasePerEnhance;
             Mathf.Clamp(runtimeStatus.finalBattleStatus.critChance, 0f, 1f); //공격속도 증가 디버프 있을수도 있어서
         }
         else{ Debug.LogWarning($"치명타 확률 SO 찾지 못함");}
@@ -68,7 +67,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 크리티컬 피해 = 기본 크리티컬 피해 + (크리티컬 피해 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalBattleStatus.critDamage =
                 runtimeStatus.baseStat.baseBattle.critDamage +
-                (runData.stat.upgrade[StatusType.CritDmg] * stat.increasePerEnhance);
+                (runProgressState.statUpgrades.upgradeLevelsByType[StatusType.CritDmg] * stat.increasePerEnhance);
         }
         else{ Debug.LogWarning($"치명타 피해 SO 찾지 못함");}
 
@@ -76,7 +75,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 이동속도 = 기본 이동속도 + (이동속도 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalBattleStatus.moveSpeed =
                 runtimeStatus.baseStat.baseBattle.moveSpeed +
-                (runData.stat.upgrade[StatusType.MoveSpeed] * stat.increasePerEnhance);
+                (runProgressState.statUpgrades.upgradeLevelsByType[StatusType.MoveSpeed] * stat.increasePerEnhance);
         }
         else{ Debug.LogWarning($"이동속도 SO 찾지 못함");}
 
@@ -84,7 +83,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 골드 획득량 = 기본 골드 획득량 + (골드 획득량 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalRewardStatus.goldRate =
                 runtimeStatus.baseStat.baseReward.goldRate +
-                (runData.stat.upgrade[StatusType.GoldRate] * stat.increasePerEnhance);
+                (runProgressState.statUpgrades.upgradeLevelsByType[StatusType.GoldRate] * stat.increasePerEnhance);
         }
         else{ Debug.LogWarning($"골드 획득량 SO 찾지 못함");}
 
@@ -92,7 +91,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 경험치 획득량 = 기본 경험치 획득량 + (경험치 획득량 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalRewardStatus.expRate =
                 runtimeStatus.baseStat.baseReward.expRate +
-                (runData.stat.upgrade[StatusType.ExpRate] * stat.increasePerEnhance);
+                (runProgressState.statUpgrades.upgradeLevelsByType[StatusType.ExpRate] * stat.increasePerEnhance);
         }
         else{ Debug.LogWarning($"경험치 획득량 SO 찾지 못함");}
 
@@ -100,7 +99,7 @@ public class StatusCalculator : MonoBehaviour
         {   //최종 아이템 드랍 확률 = 기본 아이템 드랍 확률 - (아이템 드랍 확률 스탯 업그레이드 수 * 업그레이드 당 증가량)
             runtimeStatus.finalRewardStatus.itemDropRateBonus =
                 runtimeStatus.baseStat.baseReward.itemDropRateBonus +
-                (runData.stat.upgrade[StatusType.ItemDropRate] * stat.increasePerEnhance);
+                (runProgressState.statUpgrades.upgradeLevelsByType[StatusType.ItemDropRate] * stat.increasePerEnhance);
         }
         else{ Debug.LogWarning($"아이템 드랍률 SO 찾지 못함");}
 
