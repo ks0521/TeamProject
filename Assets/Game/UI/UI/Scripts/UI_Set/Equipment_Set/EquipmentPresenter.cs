9378b0e7bcfd171bc3999d1ac64cfc6acbf8e44a
GAWN GYU <rhksrb0823@gmail.com>
2026-03-31 00:55:29 +0900

    3월 30일 작업

--- Assets/Game/UI/UI/Scripts/UI_Set/Equipment_Set/EquipmentPresenter.cs
+++ Assets/Game/UI/UI/Scripts/UI_Set/Equipment_Set/EquipmentPresenter.cs
@@ -1,3 +1,4 @@
+using Base.Manager;
 using Base.Managers;
 using Base.Save;
 using Growth.Equipment;
@@ -10,7 +11,10 @@ using UnityEngine.UI;
 public class EquipmentPresenter : MonoBehaviour, IManager
 {
 
-    [SerializeField] EquipmentDictionarySO equipment;
+    private EquipmentManager equipment;
+
+    [Header("상세창")]
+    [SerializeField] private EquipmentDetailView detailView;
 
     [Header("장비 버튼")]
     [SerializeField] private Button weaponBtn;
@@ -22,41 +26,62 @@ public class EquipmentPresenter : MonoBehaviour, IManager
     [SerializeField] private GameObject armorPop;
     [SerializeField] private GameObject accPop;
     int currentTab = 0;
+
+    private EquipmentSlotView[] weaponSlots;
+    private EquipmentSlotView[] armorSlots;
+    private EquipmentSlotView[] accSlots;
+
     private void OnEnable()
     {
+        detailView.SetActive(false);
         ShowPopup(currentTab);
     }
     public void Init()
     {
-       
+        equipment = GameManager.Instance.GetGameSystem<EquipmentManager>();
+
+        weaponSlots = weaponPop.GetComponentsInChildren<EquipmentSlotView>(true);
+        //armorSlots = armorPop.GetComponentsInChildren<EquipmentSlotView>(true);
+        //accSlots = accPop.GetComponentsInChildren<EquipmentSlotView>(true);
+
         weaponBtn.onClick.AddListener(() => ShowPopup(0));
         armorBtn.onClick.AddListener(() => ShowPopup(1));
         accBtn.onClick.AddListener(() => ShowPopup(2));
+
+        gameObject.SetActive(false);
     }
-    public int GetOrder() => 220;
+    public int GetOrder() => 230;
     void ShowPopup(int index)
     {
-        weaponPop.SetActive(false);
-        armorPop.SetActive(false);
-        accPop.SetActive(false);
+        weaponPop.SetActive(index == 0);
+        armorPop.SetActive(index == 1);
+        accPop.SetActive(index == 2);
 
         currentTab = index;
+        detailView.SetActive(false);
 
-        switch (index)
-        {
-            case 0:
-                weaponPop.SetActive(true);
-                break;
-
-            case 1:
-                armorPop.SetActive(true);
-                break;
-
-            case 2:
-                accPop.SetActive(true);
-                break;
-        }
+        RefreshPopup();
     }
 
+    void RefreshPopup()
+    {
+        switch (currentTab)
+        {
+            case 0:
+                BindSlots(EquipType.Weapon, weaponSlots);
+                break;
+                /*case 1:
+                    BindSlots(EquipType.Armor, armorSlots);
+                    break;
+                case 2:
+                    BindSlots(EquipType.Accessory, accSlots);
+                    break;*/
+        }
+    }//새로고침 할 팝업
+    void BindSlots(EquipType equipType, EquipmentSlotView[] slots)
+    {
 
+
+
+    }
 }
