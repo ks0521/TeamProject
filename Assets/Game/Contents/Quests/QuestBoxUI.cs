d520ed5b90d117ab830474d1eedc69566d66094e
AlcheJ <alchej@naver.com>
2026-04-07 19:54:34 +0900

    260407 퀘스트 진척 사항
    -업적에 사용할 무한 퀘스트 로직 구현. 레벨 n 달성 등 값이 누적되는 형태와, 스킬 n회 사용 등 완료 시마다 요구량만큼 값을 차감하는 형태를 구분하여, 퀘스트를 1번 완료할 때마다 처음 요구치의 n배만큼 요구량을 증가시키도록 함.
    -모두 완료 버튼을 누르면 연속으로 완료할 수 있는 업적을 한번에 완료하는 기능 확립
    -같은 퀘스트가 수치만 달라질 경우, 프리팹 1칸만 사용하도록 변경.

--- Assets/Game/Contents/Quests/QuestBoxUI.cs
+++ Assets/Game/Contents/Quests/QuestBoxUI.cs
@@ -8,13 +8,22 @@ using UnityEngine.UI;
 public class QuestBoxUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TextMeshProUGUI statusText;
     [SerializeField] private TextMeshProUGUI progressText;
     [SerializeField] private Image background; // 하이라이트용 배경
     [SerializeField] private GameObject redDot; // 박스 내부 레드닷
 
+    private Color highlightColor;
+    private Color normalColor;
+
     private ActiveQuest _quest;
     private QuestUIManager _uiManager;
     public ActiveQuest GetQuest() => _quest;
+    void Awake()
+    {
+        ColorUtility.TryParseHtmlString("#74A9CD", out highlightColor);
+        ColorUtility.TryParseHtmlString("#668499", out normalColor);
+    }
     public void Setup(ActiveQuest quest, QuestUIManager uiManager)
     {
         if (quest == null) return;
@@ -22,11 +31,9 @@ public class QuestBoxUI : MonoBehaviour
         _quest = quest;
         _uiManager = uiManager;
 
-        // 데이터 반영
-        titleText.text = quest.Data.description;
-        progressText.text = $"{quest.CurrentValue}/{quest.Data.targetValue}";
+        titleText.text = quest.RuntimeDescription;
+        progressText.text = $"{quest.CurrentValue} / {quest.RuntimeTargetValue}";
 
-        // 버튼 클릭 이벤트 연결
         GetComponent<Button>().onClick.AddListener(() => _uiManager.SelectQuest(_quest));
 
         if (redDot != null) RefreshVisuals();
@@ -35,11 +42,12 @@ public class QuestBoxUI : MonoBehaviour
     {
         if (_quest == null) return;
 
-        // 1. 진행도 텍스트 갱신 (예: 10/100)
+        //진행도 텍스트 갱신 (예: 10/100)
         if (progressText != null)
-            progressText.text = $"{_quest.CurrentValue} / {_quest.Data.targetValue}";
-
-        // 2. 완료 여부에 따른 레드닷 표시
+            progressText.text = $"{_quest.CurrentValue} / {_quest.RuntimeTargetValue}";
+        //퀘스트 진행 상태
+        if (statusText != null) statusText.text = $"({_quest.GetStatusText()})";
+        //완료 여부에 따른 레드닷 표시
         if (redDot != null)
             redDot.SetActive(_quest.isCompleted);
     }
@@ -47,6 +55,6 @@ public class QuestBoxUI : MonoBehaviour
     public void SetHighlight(bool isSelected)
     {
         if (background != null)
-            background.color = isSelected ? new Color(0.7f, 0.9f, 0.7f) : Color.white;
+            background.color = isSelected ? highlightColor : normalColor;
     }
 }
